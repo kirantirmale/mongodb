4c2aaa6cd13041e8fa431255dae8bca5eb1747c4
Kiran Tirmale <127860308+kirantirmale@users.noreply.github.com>
2023-07-24 07:36:43 +0530

    done

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const app = express()
 const port = 3000
 const router = require('./router/index')
 const {errorHandler} = require("./utils/errarHandler")
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger/mysql.json');
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/api', router)
 app.use(errorHandler)
